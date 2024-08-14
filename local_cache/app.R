6e13bb3078638f8f00ae8d573b6e529761d85c86
Remy Gavard <remy.gavard@appsilon.com>
2024-08-14 16:19:47 +0800

    feat: add local cache example

---
+++ local_cache/app.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+source("../generate_gwas_data.R")
+source("../manhattan_plot.R")
+shinyOptions(cache = cachem::cache_disk("./app_cache/cache/"))
+
+# UI
+ui <- fluidPage(
+  titlePanel("Manhattan Plot"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("n_points", "Number of Points:", min = 500000, max = 3000000, value = 500000, step = 500000)
+    ),
+    mainPanel(
+      plotOutput("manhattanPlot", width = 600, height = 400)
+    )
+  )
+)
+ 
+# Server
+server <- function(input, output, session) {
+  data <- reactive({
+    generate_gwas_data(input$n_points)
+  }) |> bindCache(input$n_points, cache = "app")
+  
+  output$manhattanPlot <- renderPlot({
+    create_manhattan_plot(data())
+  }) |> bindCache(input$n_points, cache = "app")
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
